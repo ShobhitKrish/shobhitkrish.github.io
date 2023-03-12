14ab82d5364955f927af4a7186046e45d87cedd8
Shobhit Krishna <126606139+ShobhitKrish@users.noreply.github.com>
2023-03-12 18:59:06 +0000

    All relevant content put on the website

--- js/main.js
+++ js/main.js
@@ -437,27 +437,59 @@ $(function() {
 });
 
 // Get the modal
+// Success Stories
 var modal1 = document.getElementById('myModal1');
 var modal2 = document.getElementById('myModal2');
 var modal3 = document.getElementById('myModal3');
 var modal4 = document.getElementById('myModal4');
 var modal5 = document.getElementById('myModal5');
 
+// Experience
+var modal6 = document.getElementById('myModal6');
+var modal7 = document.getElementById('myModal7');
+var modal8 = document.getElementById('myModal8');
+var modal9 = document.getElementById('myModal9');
+var modal10 = document.getElementById('myModal10');
+var modal11 = document.getElementById('myModal11');
+
+
+
 // Get the button that opens the modal
+// Success Stories
 var btn1 = document.getElementById("myBtn1");
 var btn2 = document.getElementById("myBtn2");
 var btn3 = document.getElementById("myBtn3");
 var btn4 = document.getElementById("myBtn4");
 var btn5 = document.getElementById("myBtn5");
 
+// Experience
+var btn6 = document.getElementById("myBtn6");
+var btn7 = document.getElementById("myBtn7");
+var btn8 = document.getElementById("myBtn8");
+var btn9 = document.getElementById("myBtn9");
+var btn10 = document.getElementById("myBtn10");
+var btn11 = document.getElementById("myBtn11");
+
+
 // Get the <span> element that closes the modal
+// Success Stories
 var span1 = document.getElementsByClassName("close1")[0];
 var span2 = document.getElementsByClassName("close2")[0];
 var span3 = document.getElementsByClassName("close3")[0];
 var span4 = document.getElementsByClassName("close4")[0];
 var span5 = document.getElementsByClassName("close5")[0];
 
-// When the user clicks the button, open the modal 
+// Experience
+var span6 = document.getElementsByClassName("close6")[0];
+var span7 = document.getElementsByClassName("close7")[0];
+var span8 = document.getElementsByClassName("close8")[0];
+var span9 = document.getElementsByClassName("close9")[0];
+var span10 = document.getElementsByClassName("close10")[0];
+var span11 = document.getElementsByClassName("close11")[0];
+
+
+// When the user clicks the button, open the modal
+// Success Stories
 btn1.onclick = function() {
     modal1.style.display = "block";
 }
@@ -474,7 +506,31 @@ btn5.onclick = function() {
     modal5.style.display = "block";
 }
 
+// Experience
+btn6.onclick = function() {
+    modal6.style.display = "block";
+}
+btn7.onclick = function() {
+    modal7.style.display = "block";
+}
+btn8.onclick = function() {
+    modal8.style.display = "block";
+}
+btn9.onclick = function() {
+    modal9.style.display = "block";
+}
+btn10.onclick = function() {
+    modal10.style.display = "block";
+}
+btn11.onclick = function() {
+    modal11.style.display = "block";
+}
+
+
+
+
 // When the user clicks on <span> (x), close the modal
+// Success Stories
 span1.onclick = function() {
     modal1.style.display = "none";
 }
@@ -491,8 +547,30 @@ span5.onclick = function() {
     modal5.style.display = "none";
 }
 
+// Experience
+span6.onclick = function() {
+    modal6.style.display = "none";
+}
+span7.onclick = function() {
+    modal7.style.display = "none";
+}
+span8.onclick = function() {
+    modal8.style.display = "none";
+}
+span9.onclick = function() {
+    modal9.style.display = "none";
+}
+span10.onclick = function() {
+    modal10.style.display = "none";
+}
+span11.onclick = function() {
+    modal11.style.display = "none";
+}
+
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
+	// Success Stories
     if (event.target == modal1) {
         modal1.style.display = "none";
     }
@@ -508,4 +586,25 @@ window.onclick = function(event) {
     if (event.target == modal5) {
         modal5.style.display = "none";
     }
+
+	// Experience
+	if (event.target == modal6) {
+        modal6.style.display = "none";
+    }
+	if (event.target == modal7) {
+        modal7.style.display = "none";
+    }
+	if (event.target == modal8) {
+        modal8.style.display = "none";
+    }
+	if (event.target == modal9) {
+        modal9.style.display = "none";
+    }
+	if (event.target == modal10) {
+        modal10.style.display = "none";
+    }
+	if (event.target == modal11) {
+        modal11.style.display = "none";
+    }
+
 }
